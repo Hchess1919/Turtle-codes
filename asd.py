d2d85fa34f13d07e569d6dcd2600b78c0145352c
Hchess1919 <hccalisgan06@gmail.com>
2023-09-23 15:07:51 +0300

    as

---
+++ asd.py
@@ -0,0 +1,5 @@
+ad ="halil"
+tel=5332112883
+
+print(ad,"in telofanu:",tel)
+print("{} in telefonu: {}".format(ad,tel))
---
+++ süüüüüüüüüüüüüüüüü.py
@@ -0,0 +1,9 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)
+i=0
+while True:
+    t.forward(i)
+    i+=0.01
+    t.left(1)
