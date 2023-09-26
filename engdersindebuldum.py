079ea81c2da17133a1c6792e36fd0fbe3d0d8a60
halil <hccalisgan06@gmail.com>
2023-09-26 19:40:43 +0300

    as

--- engdersindebuldum.py
+++ engdersindebuldum.py
@@ -2,6 +2,7 @@ import turtle as t
 import random
 import time
 
+t.hideturtle()
 siyah='black'
 beyaz='white'
 
@@ -15,9 +16,34 @@ t.pendown()
 
 t.begin_fill()
 for a in range(2):
-    t.forward(130)
+    t.forward(200)
     t.right(90)
-    t.forward(500)
+    t.forward(550)
     t.right(90)
+
+
+
+    
 t.end_fill()
+t.begin_fill()
+t.forward(400)
+t.color('yellow')
+for a in range(2):
+    t.right(90)
+    t.forward(550)
+    t.right(90)
+    t.forward(200)
+t.end_fill()
+
+
+t.color('red')
+t.forward(200)
+t.begin_fill()
+for a in range(2):
+    t.right(90)
+    t.forward(550)
+    t.right(90)
+    t.forward(200)
+t.end_fill()
+
 t.done()
